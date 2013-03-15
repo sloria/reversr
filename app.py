0bb7a2d8744fc0b2ccc6def6362c0ae10fce8013
Steven Loria <sloria1@gmail.com>
2013-03-15 18:33:03 -0500

    Fix port setting

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template
 
 # Configuration
@@ -12,4 +13,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
